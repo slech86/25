650cb2110063bcfa3a0974a5a13b376076a71ed4
sl <slech86@gmail.com>
2023-02-05 14:43:30 +0200

    05.02.2023

---
+++ Page_Object_Model/tests/company/company_add_edit/__init__.py
@@ -0,0 +1,1 @@
+# pytest --reruns 1 --html=./reports/report.html -s tests/company/company_add_edit
---
+++ Page_Object_Model/tests/job_seeker/job_seeker_add_edit/__init__.py
@@ -0,0 +1,1 @@
+# pytest --reruns 1 --html=./reports/report.html -s tests/job_seeker/job_seeker_add_edit
