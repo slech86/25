9c816e3bb22f53644d13c49da000f100b839b5e5
sl <slech86@gmail.com>
2023-03-12 13:13:14 +0200

    12.03.2023

--- Page_Object_Model/tests/password/password_recovery/test_password_recovery.py
+++ Page_Object_Model/tests/password/password_recovery/test_password_recovery.py
@@ -1,24 +1,12 @@
 import time
 import pytest
 from Page_Object_Model.pages.site.oll_page import OllPage
-from Page_Object_Model.pages.site.job_seeker_personal_cabinet_page import JobSeekerPersonalCabinetPage
 from Page_Object_Model.configuration import UrlStartPage, UrlStartPageAdmin
-from Page_Object_Model.pages.site.company_personal_cabinet_page import CompanyPersonalCabinetPage
-from Page_Object_Model.pages.site.my_vacancies_page import MyVacanciesPage
-from Page_Object_Model.pages.site.resume_page import ResumePage
-from Page_Object_Model.pages.site.vacancies_page import VacanciesPage
-from Page_Object_Model.pages.site.vacancy_page import VacancyPage
-from Page_Object_Model.pages.site.my_responses_page import MyResponsesPage
-from Page_Object_Model.pages.site.responses_to_vacancy_page import ResponsesToVacancyPage
-from Page_Object_Model.data_for_testing import contact_display_when_response_to_vacancy
-from Page_Object_Model.pages.admin_panel.admin_page import AdminPage
-from Page_Object_Model.pages.admin_panel.admin_sql_page import AdminSqlPage
-from Page_Object_Model.users import users_variables
 from Page_Object_Model.users import Accounts
 from Page_Object_Model.pages.email_page import EmailPage
 from Page_Object_Model.pages.site.main_page import MainPage
 
-# pytest --reruns 1 --html=./reports/report.html -s tests/password_recovery
+# pytest --reruns 1 --html=./reports/report.html -s tests/password/password_recovery
 
 user = 'job_seeker_change_password'
 new_password = None
